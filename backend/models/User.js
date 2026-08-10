2dc4a3d5b804a858a97512c57ab778cfc26431ab
Jyotiranjan Jena <jyotiranjanjena58@gmail.com>
2026-08-10 16:09:09 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, lowercase: true },
     mobile: { type: String, required: true, minlength: 10, maxlength: 10 },
     password: { type: String, required: true },
-    role: { type: String, default: 'user' } // Strict role
+    role: { type: String, default: 'user' } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
