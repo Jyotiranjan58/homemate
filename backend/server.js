aaf3fc3e349315cce70a6c86358824072a56f348
Jyotiranjan Jena <jyotiranjanjena58@gmail.com>
2026-08-20 11:11:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,4 @@ mongoose
       console.log(`🚀 Server running on http://localhost:${PORT}`),
     );
   })
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+  .catch((err) => console.error("MongoDB Connection Error:", err));
